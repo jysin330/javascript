4e5b9233ff7bd13518ad5304099d3803e065ec5f
Jyoti <jyotisingh82000@gmail.com>
2024-01-09 22:17:19 +0530

    variables Revision

---
+++ 01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 1265313
+let accountEmail = "jyotisingh82000@gmail.com"
+var accountPassword = "8214741"
+accountCity = "jaipur"
+let accountState;
+
+// accountId = 87306131 // not allowed
+accountEmail = "js@gmail.com"
+accountPassword = "hwg2gy23"
+accountCity = "Delhi"
+console.table([accountId,accountEmail,accountPassword, accountCity, accountState])
+
+// Prefered Not to use var , because of issue in block scope and functional scope
