8505c1a243f7acd624ab52e0d3c331b54ad9f533
Jyoti <jyotisingh82000@gmail.com>
2024-01-09 23:14:34 +0530

    Conversion of datatype

---
+++ 03_conversionOperation.js
@@ -0,0 +1,40 @@
+// let score = "33absd" //NaN
+// let valueInNumber = Number(score) //class based
+// console.log(typeof valueInNumber); //number
+// console.log( valueInNumber); //NaN
+// let score = null
+
+
+// let score = "33"
+// console.log(typeof score);
+// console.log(typeof(score)); // as a method
+
+
+// let score = null
+// let valueInNumber = Number(score) 
+// console.log(typeof valueInNumber); //number
+// console.log( valueInNumber); // 0
+
+
+
+// note converting to number and the result--->
+
+// "33" = 33
+// "33swy" = NaN
+//  true =1, false =0
+
+
+// note converting to Boolean and the result--->
+// let isLoggedIn = 1
+// let booleanLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanLoggedIn)
+
+// 1 -> true; 0 -> false
+// "" -> false
+// "hbwbyd" -> true
+
+
+// note converting to String and the result
+let someNumber = 33
+let stringSomeNumber = String(someNumber)
+console.log(stringSomeNumber)
