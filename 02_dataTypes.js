fccc5550ce5c97dca990d642a31497ade8a474b6
Jyoti <jyotisingh82000@gmail.com>
2024-01-09 22:43:00 +0530

    Data Types Revision

---
+++ 02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // using nodejs not Browser
+//  Original Documentation - (tc39.es) ECMAscript
+
+// console.log(3+3); 
+// console.log("jyoti")
+
+let name = "jyoti"
+let age = 23
+let isLoggedIn = true
+
+// primitive DataTypes ---> 
+// 1) number -> 2 to power 53
+// 2) bigInt
+// 3) string -> ""
+// 4) null -> standalone value
+// 5) undefined
+// 6) boolean -> true/false
+// 7) symbol -> unique
+
+
+// object --->
+console.log(typeof undefined) // undefined
+console.log(typeof null) // object
