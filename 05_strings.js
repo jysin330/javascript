aa27e27d39ee2eedd8dd7ef5feabe68f9b48c858
Jyoti <jyotisingh82000@gmail.com>
2024-01-21 00:06:57 +0530

    string methods

---
+++ 05_strings.js
@@ -0,0 +1,23 @@
+const name= "   jyoti   "
+// console.log(typeof name) // string
+const name2 =new String("Roshni singh")
+// console.log(typeof name2) //object
+// console.log(`My name is ${name} and ${name2}`) // String interpolation
+// console.log(name2[0])
+// console.log(name2.__proto__)
+// console.log(name2.length)
+// console.log(name2.toUpperCase())
+// console.log(name2.charAt(2))
+// console.log(name2.indexOf("o"))
+// console.log(name2.substring(0,3))
+// console.log(name2.slice(-5,3))
+// console.log(name)
+// console.log(name.trim())
+
+let url ="https://jyoti.com/Jyoti%20SIngh"
+let url2= url.replace("%20","-")
+console.log(url2)
+console.log(url2.includes("sinros"))
+console.log(name2.split(" "))
+
+
